a92a7bc540d5532b4d8617d21a63ca13a6c58d42
Mohsen Rahmanikivi <89780859+mohsenrahmanikivi@users.noreply.github.com>
2023-09-06 20:15:10 +0200

    Update guestApp.py
    bug fix

--- tools/guestApp.py
+++ tools/guestApp.py
@@ -5,37 +5,47 @@ from bitcoinutils.setup import setup
 from bitcoinutils.hdwallet import HDWallet
 from bitcoinutils.utils import to_satoshis
 from bitcoinutils.transactions import Transaction, TxInput, TxOutput
-from bitcoinutils.keys import P2pkhAddress
+from bitcoinutils.keys import P2pkhAddress , P2shAddress
 from bitcoinutils.script import Script
 
 
-#Guest Private Key to sign the transaction
+#[CHANGE with your data]Contract detailes including RedeemScript and TxIN ID + TxIn Index
+TxIn_id= "c6af4bb99d252efe8d84d33cb0abc12756ea07391f54ac8bc513f9e583896bb0"
+TxIn_index= 0
+P2shRedeem= ['OP_DUP', 'OP_HASH160', '2834725421aa6f0dd62ef3f71ca88cb72c4ef3c9', 'OP_EQUAL', 
+             'OP_IF', 'OP_CHECKSIG', 
+             'OP_ELSE', 'OP_DUP', 'OP_HASH160', '508bc12259f07e979e752ffc315981a7c9d326e4', 'OP_EQUALVERIFY', 'OP_CHECKSIG', 
+             'OP_ENDIF'
+            ]
+            
+
+#[CHANGE with your data]Guest Private Key to signe the transaction
 guestXprv="tprv8ZgxMBicQKsPf8FCmuNKDHobFZRFq6vM7Gt5mE2s9hnS71vNcN6fJbb7KDXdahL7sZNqoyPktfkRdjVUw8v7aJRxw6Yp96tmjWzQKTCpTRo"
 derivePath="m/0"
 
-#Owner address to receive the change
+#[CHANGE with your data]Owner address to recive the change
 ownerAddr="mjBY9nEHqQsSVhy4J821vZQNL1sot71JZv"
 
-#Contract details including RedeemScript and TxIN ID + TxIn Index
-P2shRedeemInHex="63049cb3dc6ab17576a914508bc12259f07e979e752ffc315981a7c9d326e488ac6704ec76dd6ab17576a9142834725421aa6f0dd62ef3f71ca88cb72c4ef3c988ac68"
-TxIn_id= "7f74f5426774fc3270b9864cf1cf7050470ed01dbe307789f62b9a12b596b64d"
-TxIn_index= 0
+
+
 
 #
 def main():
-    # Always remember to setup the network
+    # always remember to setup the network
     setup('testnet')
 
     # 1- P2SH_Redeem  Script + add OP_1 
-    redeemScript = Script.from_raw(P2shRedeemInHex)
-    redeemScript=Script(['OP_1', redeemScript.to_hex()])
-
+   # redeemScript = Script.from_raw(P2shRedeemInHex)
+    redeemScript = Script(P2shRedeem)
     print('Redeem script:', redeemScript.get_script())
+    print('Redeem script in HEX:', redeemScript.to_hex())
+    scriptAddress =  P2shAddress.from_script(redeemScript)
+    print('Redeem script address hex: ', scriptAddress.to_string() )
+    print('Redeem script address hash160:', scriptAddress.to_hash160() )
    
-   
-    # 2- Create TxOut
-    addr = P2pkhAddress(ownerAddr)
-    txout = TxOutput(to_satoshis(0), addr.to_script_pub_key() )
+    # 2- Create Change TxOut
+    changeAddr = P2pkhAddress(ownerAddr)
+    change_txout = TxOutput(to_satoshis(0), changeAddr.to_script_pub_key() )
 
     
     # 3- Create TxIn
@@ -43,23 +53,23 @@ def main():
    
 
     # 4- Prepare the key 
-    hdw = HDWallet(guestXprv, derivePath)
-    privkey = hdw.get_private_key()
-    pubkey = privkey.get_public_key()
-    addr1 = pubkey.get_address()
-    print('Guest  address:', addr1.to_string() )
+    hdw  = HDWallet(guestXprv, derivePath)
+    prv  = hdw.get_private_key()
+    pub  = prv.get_public_key()
+    addr = prv.get_public_key().get_address()
 
     # 5- Form the TX
-    tx = Transaction([txin], [txout])
+    tx = Transaction([txin], [change_txout])
     
     # 6- Sign the TX
-    sig = privkey.sign_input(tx, 0, redeemScript )
-
-    
-    txin.script_sig = Script([sig, redeemScript.to_hex()])
+    sig = prv.sign_input(tx, 0, redeemScript)
+    txin.script_sig = Script([sig, pub.to_hex() , redeemScript.to_hex()])
     signed_tx = tx.serialize()
 
-    # print raw signed transaction ready to be broadcasted
+    print('\nscript_sig script           :', txin.script_sig.get_script())
+    print('\nGuest address(signer) base58:', addr.to_string() ) 
+    print('Guest address(signer) hash160 :', addr.to_hash160() )
+    print('Public key                    :', pub.to_hex()) 
     print("\nRaw signed transaction:\n" + signed_tx)
     print("\nTxId:", tx.get_txid())
 
