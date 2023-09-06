3a380dc1c3066023d744ef4c6228413f6cc5e241
Mohsen Rahmanikivi <89780859+mohsenrahmanikivi@users.noreply.github.com>
2023-09-06 20:15:46 +0200

    Update ownerApp.py
    bug fix

--- tools/ownerApp.py
+++ tools/ownerApp.py
@@ -6,27 +6,27 @@ from bitcoinutils.setup import setup
 from bitcoinutils.hdwallet import HDWallet
 from bitcoinutils.utils import to_satoshis
 from bitcoinutils.transactions import Transaction, TxInput, TxOutput
-from bitcoinutils.keys import P2pkhAddress, PrivateKey, Address
+from bitcoinutils.keys import P2pkhAddress, PrivateKey, Address, P2shAddress
 from bitcoinutils.script import Script
 
-#Contract details including RedeemScript and TxIN ID + TxIn Index
-TxIn_id= "b357b6bae552f99483d0c08ee101704101cbbf3cc3c84c00ffa4ea79ff89b69f"
-TxIn_index=1 
-fund=0.0000635
-fee=350/100000000
+#[CHANGE with your data]Contract details including RedeemScript and TxIN ID + TxIn Index
+TxIn_id= "c6af4bb99d252efe8d84d33cb0abc12756ea07391f54ac8bc513f9e583896bb0"
+TxIn_index= 2 
+fund= 0.00011372
+fee= 350/100000000
 
-#Owner MASTER Private Key to sign the transaction
+#[CHANGE with your data]Owner MASTER Private Key to sign the transaction
 ownerXprv="tprv8ZgxMBicQKsPdJoBnWQ4NXgfYY3a344cVpfxGVVAMex4Ka5UZfdcEVC8E43cpxpj9WfzWJLy8yRQWzD5StoRm6JLqjLNCbfyKsDEmqX3Lhh"
 derivePath="m/0"
-ownerTime=1682848796
+ownerTime=123
 
-#Lock address 
+#[CHANGE with your data]Lock address 
 lockAddr="n4Epcra9y3WigWeaDJxMxYNvmPeS7eEDwR"
 
 
-#Guest Address
+#[CHANGE with your data]Guest Address
 guestAddr="mnrqmi3gyEavAtv1hFMxqWjRcSzvPzJBnm"
-guestTime=1693586157
+guestTime=123
 
 
 
@@ -48,34 +48,42 @@ def main():
     privkey = hdw.get_private_key()
     pubkey = privkey.get_public_key()
     ownerAddr = pubkey.get_address()
-    print('\nOwner  address:\n', ownerAddr.to_string() )
+    print('\nOwner  address:', ownerAddr.to_string() )
 
     guestAddrString = P2pkhAddress(guestAddr)
     lockAddrString = P2pkhAddress(lockAddr)
 
-    # 1- Prepare the redeem script
-    p2shRedeem=['OP_IF'     , guestTimeHexBig[:8] , 'OP_CHECKLOCKTIMEVERIFY', 'OP_DROP', \
-                'OP_DUP'    , 'OP_HASH160', guestAddrString.to_hash160(),'OP_EQUALVERIFY', 'OP_CHECKSIG',\
-                'OP_ELSE'   , ownerTimeHexBig[:8], 'OP_CHECKLOCKTIMEVERIFY', 'OP_DROP',\
-                'OP_DUP'    , 'OP_HASH160', ownerAddr.to_hash160(), 'OP_EQUALVERIFY', 'OP_CHECKSIG', 'OP_ENDIF']
+    
+    p2shRedeem=[
+                'OP_DUP'    , 'OP_HASH160'  , guestAddrString.to_hash160(),'OP_EQUAL', 
+                'OP_IF'     , 'OP_CHECKSIG' ,
+                'OP_ELSE'   , 'OP_DUP'      , 'OP_HASH160', ownerAddr.to_hash160(), 'OP_EQUALVERIFY', 'OP_CHECKSIG'  ,
+                'OP_ENDIF'   
+                ]
+    
    
-   
-    # 2.1- Create a TxOut P2SH
+    # 2.1- Create a TxOut with P2SH
     redeemScript=Script(p2shRedeem)
-    print('\nRedeem script:\n', redeemScript.get_script())
+
+    
+    print('Redeem script:', redeemScript.get_script())
+    print('Redeem script in HEX:', redeemScript.to_hex())
+    scriptAddress =  P2shAddress.from_script(redeemScript)
+    print('Redeem script address hex: ', scriptAddress.to_string() )
+    print('Redeem script address hash160:', scriptAddress.to_hash160() )
     P2SH_txout = TxOutput(  to_satoshis(fee), redeemScript.to_p2sh_script_pub_key() )
     
     
-    # 2.2- Create a TxOut OP_Return
+    # 2.2- Create a TxOut with OP_Return
     opReturn = p2shRedeem
     opReturn.insert( 0, 'OP_RETURN')
     OpreturnScript= Script( opReturn )
     OPRUTURN_txout = TxOutput(  to_satoshis(0), OpreturnScript) 
-    print('\nOP_Return script:\n', OpreturnScript.get_script())
+    print('\nOP_Return script:', OpreturnScript.get_script())
 
-    # 2.3- Create a TxOut P2PKH (change output)
-    addr = P2pkhAddress(lockAddr)
-    P2PKH_txout = TxOutput( to_satoshis(fund - fee - fee) , addr.to_script_pub_key() )
+    # 2.3- Create a TxOut with P2PKH (change output)
+    changeAddr = P2pkhAddress(lockAddr)######################################################################################################address for tx
+    P2PKH_txout = TxOutput( to_satoshis(fund - fee - fee) , ownerAddr.to_script_pub_key() )
     
     # 3- Create a TxIn
     txin = TxInput(TxIn_id , TxIn_index)
